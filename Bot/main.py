b30cb7b367e62a1720c08af7179c39d7ee83f9a0
Ser11y <20-Ser11y@users.noreply.git.arumoon.ru>
2023-05-14 23:08:59 +0300

    Константы выведены в отдельный файл.

--- Bot/main.py
+++ Bot/main.py
@@ -1,13 +1,13 @@
 ﻿from INFO import TOKEN  # Импортируем значение TOKEN из файла INFO.py
 from INFO import PREFIX  # Импортируем значение PREFIX из файла INFO.py
+from NAMES import c_names, c_descs
+import random
+import aiohttp
 import discord
-import time
+import asyncio
 from discord.ext import commands
-from discord import app_commands
-
 
 # INITIALIZATION
-
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -15,36 +15,18 @@ bot = discord.ext.commands.Bot(intents=intents,
                                command_prefix=PREFIX,
                                help_command=None)  # Установка Prefix.
 
-
 # INITIALIZATION END
-
 
 # EVENTS
 
 
 @bot.event
 async def on_ready():
-    await bot.change_presence(activity=discord.Game(name=f'Prefix: "{bot.command_prefix}"'))
+    await bot.change_presence(activity=discord.Game(
+        name=f'Prefix: "{bot.command_prefix}"'))
 
 
 # EVENTS END
-
-
-# ARRAYS
-
-commands_names = ["0",
-                  "prefix",
-                  "help",
-                  "clear",
-                  "user"]
-commands_descriptions = ["0",
-                         "Изменить Prefix для команд.",
-                         "Выводит названия и описания всех команд.",
-                         "Удаляет введённое кол-во сообщений.",
-                         "Выводит информацию об пользователе."]
-
-
-# ARRAYS END
 
 
 # COMMANDS
@@ -61,20 +43,22 @@ async def prefix(ctx, new_prefix: str):
 
 @bot.command()
 async def help(ctx):
-    emb1 = discord.Embed(title='Помощь',
-                         description=f'В настоящий момент, prefix - "{bot.command_prefix}". Список возможных команд:',
-                         color=0x00FF00)
-    emb1.add_field(name=commands_names[1],
-                   value=commands_descriptions[1],
+    emb1 = discord.Embed(
+        title='Помощь',
+        description=
+        f'В настоящий момент, prefix - "{bot.command_prefix}". Список возможных команд:',
+        color=0x00FF00)
+    emb1.add_field(name=c_names[1],
+                   value=c_descs[1],
                    inline=False)
-    emb1.add_field(name=commands_names[2],
-                   value=commands_descriptions[2],
+    emb1.add_field(name=c_names[2],
+                   value=c_descs[2],
                    inline=False)
-    emb1.add_field(name=commands_names[3],
-                   value=commands_descriptions[3],
+    emb1.add_field(name=c_names[3],
+                   value=c_descs[3],
                    inline=False)
-    emb1.add_field(name=commands_names[4],
-                   value=commands_descriptions[4],
+    emb1.add_field(name=c_names[4],
+                   value=c_descs[4],
                    inline=False)
     await ctx.send(embed=emb1)
 
@@ -90,9 +74,8 @@ async def clear(ctx, amount: int):
                          description=f'Было удалено {amount} сообщений.',
                          color=0x00FF00)
     await ctx.send(embed=emb1)
-    time.sleep(3)
-    await ctx.channel.purge(limit=50,
-                            check=is_me)
+    asyncio.sleep(3)
+    await ctx.channel.purge(limit=50, check=is_me)
 
 
 @bot.command()
@@ -107,21 +90,28 @@ async def user(ctx, member: discord.Member):
     embed.add_field(name="Имя:",
                     value=f'{member.name}#{member.discriminator}',
                     inline=False)
-    embed.add_field(name="Присоединился:",
-                    value=joined,
-                    inline=False)
-    embed.add_field(name="Создан:",
-                    value=created,
-                    inline=False)
-    embed.add_field(name="Роли:",
-                    value=", ".join(roles),
-                    inline=False)
+    embed.add_field(name="Присоединился:", value=joined, inline=False)
+    embed.add_field(name="Создан:", value=created, inline=False)
+    embed.add_field(name="Роли:", value=", ".join(roles), inline=False)
 
     await ctx.send(embed=embed)
 
 
-# COMMANDS END
+@bot.command()
+async def meme(ctx):
+    async with aiohttp.ClientSession() as session:
+        async with session.get('https://www.reddit.com/r/memes.json') as r:
+            if r.status == 400:
+                data = await r.json()
+                post = random.choice(data['data']['children'])['data']
+                embed = discord.Embed(title=post['title'],
+                                      description='',
+                                      color=0x00FF00)
+                embed.set_image(url=post['url'])
+                await ctx.send(embed=embed)
 
+
+# COMMANDS END
 
 # ERRORS
 
@@ -135,8 +125,7 @@ async def clear_error(ctx, error):
 @user.error
 async def user_error(ctx, error):
     if isinstance(error, commands.CommandError):
-        embed = discord.Embed(title="Ошибка!",
-                              color=0xFF0000)
+        embed = discord.Embed(title="Ошибка!", color=0xFF0000)
         embed.add_field(name="Код ошибки:", value=str(error))
 
         await ctx.send(embed=embed)
