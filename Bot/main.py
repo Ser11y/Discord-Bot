787082ee79b766f9577ff1d95f9940945e6c514b
Ser11y <20-Ser11y@users.noreply.git.arumoon.ru>
2023-05-14 20:06:31 +0300

    Added base

---
+++ Bot/main.py
@@ -0,0 +1,17 @@
+﻿from TOKEN import TOKEN
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = discord.ext.commands.Bot(intents=intents, command_prefix="!")
+
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send("Hello!")
+
+
+print(TOKEN)
+
+bot.run(TOKEN)
